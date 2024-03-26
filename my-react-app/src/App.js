159c8f01d988664e9592e974f4135cde445df7f9
Abdinasir <samiradynassir@gmail.com>
2024-03-27 00:51:57 +0300

    updated navbar and student form

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,7 @@ import RegForm from './components/RegForm';
 import StudentForm from './components/StudentForm';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
